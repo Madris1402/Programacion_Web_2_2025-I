cf19d16962b1a41c2fce7da5363135ffa0ee3f7e
Ricardo Madrigal <ricmad142@gmail.com>
2024-10-10 09:59:53 -0600

    Calculadora con Elementos DOM Rev2

--- Clases/Clase 07/app4.js
+++ Clases/Clase 07/app4.js
@@ -1,14 +1,3 @@
-document.getElementById('sumForm').addEventListener('submit', function(event){
-
-    event.preventDefault();
-    let numero1 = parseInt(document.getElementById('num1').value);
-    let numero2 = parseInt(document.getElementById('num2').value);
-    let resultado = numero1 + numero2;
-
-    document.getElementById('resultado').innerText = 'La suma es: ' + resultado;
-});
-
-
 document.getElementById('calculator-form').addEventListener('submit', function(event) {
     event.preventDefault();
 
