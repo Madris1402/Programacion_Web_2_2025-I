60c7e0c76d7112118feb3d7dfc33057ad1413145
Ricardo Madrigal <ricmad142@gmail.com>
2024-10-24 10:43:58 -0600

    Rutas en Node Express

---
+++ Clases/Clase 11/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+const pageRutes = require("./routes/pages");
+
+const app = express();
+const PORT = 3059;
+
+app.use(express.static('public'));
+
+app.use('/', pageRutes);
+
+app.get('/',(req, res) => {
+    res.redirect('/page1')
+});
+
+app.listen(PORT, () => {
+    console.log(`Escuchando desde http://127.0.0.1:${PORT}`);
+});
